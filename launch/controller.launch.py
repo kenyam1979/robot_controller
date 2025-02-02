19f08d632103db9f4e849cb3bbc9dcce9e2af9da
kenyam1979 <ken.yamaji@gmail.com>
2025-02-02 18:53:51 +0900

    20250202

--- launch/controller.launch.py
+++ launch/controller.launch.py
@@ -20,29 +20,29 @@ def generate_launch_description():
 
     controller_pkg_share = launch_ros.substitutions.FindPackageShare(package='robot_controller').find('robot_controller')
     lidar_pkg_share = launch_ros.substitutions.FindPackageShare(package='sllidar_ros2').find('sllidar_ros2')
-    imu_pkg_share = launch_ros.substitutions.FindPackageShare(package='wit_ros2_imu_fixed').find('wit_ros2_imu_fixed')
+    imu_pkg_share = launch_ros.substitutions.FindPackageShare(package='wit_ros2_imu').find('wit_ros2_imu')
     description_pkg_share = launch_ros.substitutions.FindPackageShare(package='robot_discription').find('robot_description')
 
     # Launch lidar sensor
     lidar_launch = IncludeLaunchDescription(
         PathJoinSubstitution(
-            [lidar_pkg_share, "laucnch", "sllidar_a1_launch.py"]))
+            [lidar_pkg_share, "launch", "sllidar_a1_launch.py"]))
 
 
     # Launch IMU sensor
     imu_launch = IncludeLaunchDescription(
         PathJoinSubstitution(
-            [imu_pkg_share, "laucnch", "imu.launch.py"]))
+            [imu_pkg_share, "launch", "imu.launch.py"]))
 
     # Launch robot_state_publisher and localization
     robot_launch = IncludeLaunchDescription(
         PathJoinSubstitution(
-            [description_pkg_share, "laucnch", "robot.launch.py"]))
+            [description_pkg_share, "launch", "robot.launch.py"]))
 
     # Launch teleop
     teleop_launch = IncludeLaunchDescription(
         PathJoinSubstitution(
-            [controller_pkg_share, "laucnch", "teleop_launch.py"]))
+            [controller_pkg_share, "launch", "teleop_launch.py"]))
 
     # Run motor_controller node
     controller_node = Node(
