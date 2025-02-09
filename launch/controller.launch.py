c129fb12888d3cc57bfa8cfbdb133284a3fbaf14
kenyam1979 <ken.yamaji@gmail.com>
2025-02-09 16:49:00 +0900

    20250208

--- launch/controller.launch.py
+++ launch/controller.launch.py
@@ -42,7 +42,7 @@ def generate_launch_description():
     # Launch teleop
     teleop_launch = IncludeLaunchDescription(
         PathJoinSubstitution(
-            [controller_pkg_share, "teleop_launch.py"]))
+            [controller_pkg_share, "teleop.launch.py"]))
 
     # Run motor_controller node
     controller_node = Node(
