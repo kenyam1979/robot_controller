b10b452436875bf2b88305d845251e04077e87a6
kenyam1979 <ken.yamaji@gmail.com>
2025-02-09 16:46:18 +0900

    20250208

--- launch/teleop.launch.py
+++ launch/teleop.launch.py
@@ -29,8 +29,10 @@ def generate_launch_description():
             'config_filepath', 
             default_value=[
                 launch.substitutions.TextSubstitution(
-                    text=os.path.join(get_package_share_directory('robot_controller'),'/')),
-                joy_config, launch.substitutions.TextSubstitution(text='.config.yaml')]),
+                    text=os.path.join(get_package_share_directory('robot_controller'))),
+                '/',
+                joy_config, 
+                launch.substitutions.TextSubstitution(text='.config.yaml')]),
 
         launch_ros.actions.Node(
             package='joy_linux', executable='joy_linux_node', name='joy_linux_node',
